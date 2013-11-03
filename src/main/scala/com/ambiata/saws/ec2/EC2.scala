e0a54fe07d7a6bf2a639209fafa07ece7083e66e
Ben Lever <ben.lever@nicta.com.au>
2013-11-03 17:12:02 +1100

    [#59537504] Port sec-group handlers from tarski.

--- src/main/scala/com/ambiata/saws/ec2/EC2.scala
+++ src/main/scala/com/ambiata/saws/ec2/EC2.scala
@@ -1,16 +1,86 @@
 package com.ambiata.saws
 package ec2
 
+import scalaz._, Scalaz._
+import scala.collection.JavaConversions._
 import com.amazonaws.services.ec2.AmazonEC2Client
+import com.amazonaws.services.ec2.model.{SecurityGroup => AwsSecurityGroup, _}
+import AwsAttempt._
+
+
+/** Wrapper for Java IAM client. */
+case class EC2(client: AmazonEC2Client) {
+
+  /** Return details for a specified security group, if it exists. */
+  def securityGroup(name: String): AwsAttempt[Option[AwsSecurityGroup]] =
+    AwsAttempt {
+      client.describeSecurityGroups().getSecurityGroups.find(_.getGroupName == name)
+    }
+
+
+  /** Create a security group. Do nothing if the security group already exists. */
+  def createSecurityGroup(group: SecurityGroup): AwsAttempt[Unit] = {
+    securityGroup(group.name) >>= { sg =>
+      AwsAttempt {
+        if (sg.isEmpty)
+          client.createSecurityGroup(new CreateSecurityGroupRequest(group.name, group.desc))
+      }
+    }
+  }
+
+
+  /** Revoke all existing permissions then apply specified ingress rules for a given security group. */
+  def updateSecurityGroupIngress(group: SecurityGroup): AwsAttempt[Unit] = {
+    securityGroup(group.name) >>= { sg =>
+      AwsAttempt {
+        sg foreach { existing =>
+          existing.getIpPermissions.map(safeIpPermissionCopy) foreach { ipp => revokeSecurityGroupIngress(group.name, ipp) }
+          group.ingressRules foreach { ipp => authorizeSecurityGroupIngress(group.name, ipp) }
+        }
+      }
+    }
+  }
+
+
+  /** Authorize an ingress rules for a given security group. */
+  def authorizeSecurityGroupIngress(group: String, ipPermission: IpPermission): AwsAttempt[Unit] = {
+    AwsAttempt {
+      client.authorizeSecurityGroupIngress(new AuthorizeSecurityGroupIngressRequest(group, List(ipPermission)))
+    }
+  }
+
+
+  /** Revoke an ingress rules for a given security group. */
+  def revokeSecurityGroupIngress(group: String, ipPermission: IpPermission): AwsAttempt[Unit] = {
+    AwsAttempt {
+      client.revokeSecurityGroupIngress(new RevokeSecurityGroupIngressRequest(group, List(ipPermission)))
+    }
+  }
+
+
+  /** Copy all fields except group and user ID as these fields are assigned by AWS so can not be used as part of
+    * a revoke request. */
+  private def safeIpPermissionCopy(ipp: IpPermission): IpPermission =
+    new IpPermission()
+      .withIpProtocol(ipp.getIpProtocol)
+      .withFromPort(ipp.getFromPort)
+      .withToPort(ipp.getToPort)
+      .withIpRanges(ipp.getIpRanges)
+      .withUserIdGroupPairs(ipp.getUserIdGroupPairs.map(ugp => (new UserIdGroupPair).withGroupName(ugp.getGroupName)))
+
+}
 
 
 /** Sydney-region EC2 client. */
 object EC2 {
   val Ec2Endpoint = "ec2.ap-southeast-2.amazonaws.com"
 
-  def apply(): AmazonEC2Client = {
+  def apply(): EC2 = {
     val c = new AmazonEC2Client()
     c.setEndpoint(Ec2Endpoint)
-    c
+    EC2(c)
   }
+
+  def create(): AmazonEC2Client =
+    EC2().client
 }
