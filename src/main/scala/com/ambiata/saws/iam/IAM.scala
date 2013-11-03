ef341a921dab1a573a82530fab618f5dda98a8a8
Ben Lever <ben.lever@nicta.com.au>
2013-11-03 15:15:17 +1100

    Add create/delete methods for IAM roles.

--- src/main/scala/com/ambiata/saws/iam/IAM.scala
+++ src/main/scala/com/ambiata/saws/iam/IAM.scala
@@ -11,6 +11,43 @@ import AwsAttempt._
 /** Wrapper for Java IAM client. */
 case class IAM(client: AmazonIdentityManagementClient) {
 
+  /** Returns true if a role with the specified name exists. */
+  def roleExists(roleName: String): AwsAttempt[Boolean] = {
+    AwsAttempt(client.listRoles.getRoles.exists(_.getRoleName == roleName))
+  }
+
+  /** Creates a new EC2 assumed role with no initial policies. */
+  def createRole(roleName: String): AwsAttempt[Unit] = {
+    val Ec2AssumedRolePolicy =
+      """|{
+         |  "Version": "2008-10-17",
+         |  "Statement": [
+         |    {
+         |      "Sid": "",
+         |      "Effect": "Allow",
+         |      "Principal": {
+         |        "Service": "ec2.amazonaws.com"
+         |      },
+         |      "Action": "sts:AssumeRole"
+         |    }
+         |  ]
+         |}""".stripMargin
+
+    val roleReq = (new CreateRoleRequest()).withRoleName(roleName).withAssumeRolePolicyDocument(Ec2AssumedRolePolicy)
+
+    roleExists(roleName) >>= { exists =>
+      if (!exists) AwsAttempt(client.createRole(roleReq))
+      else         AwsAttempt(())
+    }
+  }
+
+  /** Delete an IAM role and all of its policies. */
+  def deleteRole(roleName: String): AwsAttempt[Unit] = {
+    clearRolePolicies(roleName) >>
+    AwsAttempt(client.deleteRole((new DeleteRoleRequest).withRoleName(roleName)))
+  }
+
+
   /** Add a policy to an IAM role. */
   def addRolePolicy(roleName: String, policy: IamPolicy): AwsAttempt[Unit] = {
     val policyReq = (new PutRolePolicyRequest())
