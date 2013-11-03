794a9e5a17c755ac0d026b45be2c5b9fc43a1f58
markhibberd <mark@hibberd.id.au>
2013-11-03 21:14:32 +1100

    [#58140262] Compose different types of actions.

--- src/main/scala/com/ambiata/saws/core/package.scala
+++ src/main/scala/com/ambiata/saws/core/package.scala
@@ -4,6 +4,8 @@ import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.ec2.AmazonEC2Client
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClient
 
+import scalaz._, Scalaz._
+
 package object core {
   type S3Action[A] = AwsAction[AmazonS3Client, A]
   type EC2Action[A] = AwsAction[AmazonEC2Client, A]
@@ -14,4 +16,24 @@ package object core {
   object EC2Action extends AwsActionTemplate[AmazonEC2Client]
   object IAMAction extends AwsActionTemplate[AmazonIdentityManagementClient]
   object S3EC2Action extends AwsActionTemplate[(AmazonS3Client, AmazonEC2Client)]
+
+  trait MonadS3[F[_]] { def liftS3[A](f: S3Action[A]): F[A] }
+  trait MonadEC2[F[_]] { def liftEC2[A](f: EC2Action[A]): F[A] }
+  trait MonadIAM[F[_]] { def liftIAM[A](f: IAMAction[A]): F[A] }
+
+  implicit def S3EC2ActionInstances: MonadS3[S3EC2Action] with MonadEC2[S3EC2Action] =
+    new MonadS3[S3EC2Action] with MonadEC2[S3EC2Action] {
+      def liftS3[A](f: S3Action[A]) = AwsAction(Kleisli({ case (s3, ec2) => f.run(s3) }))
+      def liftEC2[A](f: EC2Action[A]) =  AwsAction(Kleisli({ case (s3, ec2) => f.run(ec2) }))
+    }
+
+  implicit class S3ActionSyntax[A](action: S3Action[A]) {
+    def liftS3[F[_]: MonadS3] = implicitly[MonadS3[F]].liftS3(action)
+  }
+  implicit class EC2ActionSyntax[A](action: EC2Action[A]) {
+    def liftEC2[F[_]: MonadEC2] = implicitly[MonadEC2[F]].liftEC2(action)
+  }
+  implicit class IAMActionSyntax[A](action: IAMAction[A]) {
+    def liftIAM[F[_]: MonadIAM] = implicitly[MonadIAM[F]].liftIAM(action)
+  }
 }
--- src/main/scala/com/ambiata/saws/s3/S3.scala
+++ src/main/scala/com/ambiata/saws/s3/S3.scala
@@ -2,6 +2,12 @@ package com.ambiata.saws
 package s3
 
 import com.amazonaws.services.s3.AmazonS3Client
+import com.ambiata.saws.core._
+import com.ambiata.mundane.io.Streams
+
+import java.io.InputStream
+
+import scalaz._, Scalaz._
 
 /** Wrapper for Java S3 client. */
 case class S3(client: AmazonS3Client)
@@ -9,10 +15,15 @@ case class S3(client: AmazonS3Client)
 /** Sydney-region S3 client. */
 object S3 {
   val S3Endpoint = "s3-ap-southeast-2.amazonaws.com"
-
   def apply(): S3 = {
     val c = new AmazonS3Client()
     c.setEndpoint(S3Endpoint)
     S3(c)
   }
+
+  def getBytes(bucket: String, key: String): S3Action[Array[Byte]] =
+    getStream(bucket, key).safely(Streams.bytes(_))
+
+  def getStream(bucket: String, key: String): S3Action[InputStream] =
+    AwsAction.withClient(_.getObject(bucket, key).getObjectContent)
 }
