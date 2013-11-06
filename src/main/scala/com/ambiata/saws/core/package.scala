1922415f7c3c479396675960aedbf4f3018e215f
markhibberd <mark@hibberd.id.au>
2013-11-07 10:19:56 +1100

    [#58140262] Added S3, EC2 IAM product.

--- src/main/scala/com/ambiata/saws/core/package.scala
+++ src/main/scala/com/ambiata/saws/core/package.scala
@@ -12,6 +12,7 @@ package object core {
   type IAMAction[A] = AwsAction[AmazonIdentityManagementClient, A]
   type S3EC2Action[A] = AwsAction[(AmazonS3Client, AmazonEC2Client), A]
   type EC2IAMAction[A] = AwsAction[(AmazonEC2Client, AmazonIdentityManagementClient), A]
+  type S3EC2IAMAction[A] = AwsAction[(AmazonS3Client, AmazonEC2Client, AmazonIdentityManagementClient), A]
 
   implicit def S3EC2ActionInstances: MonadS3[S3EC2Action] with MonadEC2[S3EC2Action] =
     new MonadS3[S3EC2Action] with MonadEC2[S3EC2Action] {
@@ -23,6 +24,13 @@ package object core {
     new MonadIAM[EC2IAMAction] with MonadEC2[EC2IAMAction] {
       def liftIAM[A](f: IAMAction[A]) = AwsAction({ case (ec2, iam) => f.run(iam) })
       def liftEC2[A](f: EC2Action[A]) =  AwsAction({ case (ec2, iam) => f.run(ec2) })
+    }
+
+  implicit def S3EC2IAMActionInstances: MonadS3[S3EC2IAMAction] with MonadIAM[S3EC2IAMAction] with MonadEC2[S3EC2IAMAction] =
+    new MonadS3[S3EC2IAMAction] with MonadIAM[S3EC2IAMAction] with MonadEC2[S3EC2IAMAction] {
+      def liftS3[A](f: S3Action[A]) = AwsAction({ case (s3, ec2, iam) => f.run(s3) })
+      def liftIAM[A](f: IAMAction[A]) = AwsAction({ case (s3, ec2, iam) => f.run(iam) })
+      def liftEC2[A](f: EC2Action[A]) =  AwsAction({ case (s3, ec2, iam) => f.run(ec2) })
     }
 
   implicit class S3ActionSyntax[A](action: S3Action[A]) {
