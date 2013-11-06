bcaf61e9226873b46a8a5433cecf841115ad0201
markhibberd <mark@hibberd.id.au>
2013-11-06 15:24:20 +1100

    [#58140262] Use region configured clients..

--- src/main/scala/com/ambiata/saws/core/Interpret.scala
+++ src/main/scala/com/ambiata/saws/core/Interpret.scala
@@ -1,25 +1,25 @@
 package com.ambiata.saws
 package core
 
-import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.ec2.AmazonEC2Client
-import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClient
+import com.ambiata.saws.s3._
+import com.ambiata.saws.ec2._
+import com.ambiata.saws.iam._
 
 object Interpret {
   type Result[A] = (Vector[AwsLog], AwsAttempt[A])
 
   def s3[A](action: S3Action[A]): Result[A] =
-    action.run(new AmazonS3Client)
+    action.run(S3().client)
 
   def ec2[A](action: EC2Action[A]): Result[A] =
-    action.run(new AmazonEC2Client)
+    action.run(EC2().client)
 
   def iam[A](action: IAMAction[A]): Result[A] =
-    action.run(new AmazonIdentityManagementClient)
+    action.run(IAM().client)
 
   def s3ec2[A](action: S3EC2Action[A]): Result[A] =
-    action.run(new AmazonS3Client -> new AmazonEC2Client)
+    action.run(S3().client -> EC2().client)
 
   def ec2iam[A](action: EC2IAMAction[A]): Result[A] =
-    action.run(new AmazonEC2Client -> new AmazonIdentityManagementClient)
+    action.run(EC2().client -> IAM().client)
 }
