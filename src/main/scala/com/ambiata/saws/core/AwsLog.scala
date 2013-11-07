b5faffbc01a019b44898f65c4bc1fe03dab5a57a
markhibberd <mark@hibberd.id.au>
2013-11-07 11:14:38 +1100

    [#58140262] Trap errors earlier.

--- src/main/scala/com/ambiata/saws/core/AwsLog.scala
+++ src/main/scala/com/ambiata/saws/core/AwsLog.scala
@@ -8,6 +8,13 @@ object AwsLog {
   case class CreateBucket(name: String) extends AwsLog
   case class CreateInstanceProfile(name: String) extends AwsLog
   case class CreateSecurityGroup(name: String) extends AwsLog
+  case class UpdateRole(name: String) extends AwsLog
+  case class UpdateBucket(name: String) extends AwsLog
+  case class UpdateInstanceProfile(name: String) extends AwsLog
+  case class UpdateSecurityGroup(name: String) extends AwsLog
   case class StartInstance(name: String) extends AwsLog
-  case class Message(message: String) extends AwsLog
+  case class StopInstance(name: String) extends AwsLog
+  case class Info(message: String) extends AwsLog
+  case class Warn(message: String) extends AwsLog
+  case class Debug(message: String) extends AwsLog
 }
--- src/main/scala/com/ambiata/saws/core/Interpret.scala
+++ src/main/scala/com/ambiata/saws/core/Interpret.scala
@@ -9,20 +9,20 @@ object Interpret {
   type Result[A] = (Vector[AwsLog], AwsAttempt[A])
 
   def s3[A](action: S3Action[A]): Result[A] =
-    action.run(S3().client)
+    action.safe.run(S3().client)
 
   def ec2[A](action: EC2Action[A]): Result[A] =
-    action.run(EC2().client)
+    action.safe.run(EC2().client)
 
   def iam[A](action: IAMAction[A]): Result[A] =
-    action.run(IAM().client)
+    action.safe.run(IAM().client)
 
   def s3ec2[A](action: S3EC2Action[A]): Result[A] =
-    action.run(S3().client -> EC2().client)
+    action.safe.run(S3().client -> EC2().client)
 
   def ec2iam[A](action: EC2IAMAction[A]): Result[A] =
-    action.run(EC2().client -> IAM().client)
+    action.safe.run(EC2().client -> IAM().client)
 
   def s3ec2iam[A](action: S3EC2IAMAction[A]): Result[A] =
-    action.run((S3().client, EC2().client, IAM().client))
+    action.safe.run((S3().client, EC2().client, IAM().client))
 }
