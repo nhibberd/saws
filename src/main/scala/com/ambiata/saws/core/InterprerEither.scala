08b8a6f9aa65830347bc1263887494adc13d7e38
markhibberd <mark@hibberd.id.au>
2013-11-11 13:22:19 +1100

    Compiling.

--- src/main/scala/com/ambiata/saws/core/InterprerEither.scala
+++ src/main/scala/com/ambiata/saws/core/InterprerEither.scala
@@ -4,25 +4,26 @@ package core
 import com.ambiata.saws.s3._
 import com.ambiata.saws.ec2._
 import com.ambiata.saws.iam._
+import scalaz._, Scalaz._, \&/._
 
 object InterpretEither {
-  type Result[A] = (Vector[AwsLog], AwsAttempt[A]
+  type Result[A] = These[String, Throwable] \/ A
 
   def s3[A](action: S3Action[A]): Result[A] =
-    action.safe.run(S3().client)
+    Interpret.s3(action)._2.run
 
   def ec2[A](action: EC2Action[A]): Result[A] =
-    action.safe.run(EC2().client)
+    Interpret.ec2(action)._2.run
 
   def iam[A](action: IAMAction[A]): Result[A] =
-    action.safe.run(IAM().client)
+    Interpret.iam(action)._2.run
 
   def s3ec2[A](action: S3EC2Action[A]): Result[A] =
-    action.safe.run(S3().client -> EC2().client)
+    Interpret.s3ec2(action)._2.run
 
   def ec2iam[A](action: EC2IAMAction[A]): Result[A] =
-    action.safe.run(EC2().client -> IAM().client)
+    Interpret.ec2iam(action)._2.run
 
   def s3ec2iam[A](action: S3EC2IAMAction[A]): Result[A] =
-    action.safe.run((S3().client, EC2().client, IAM().client))
+    Interpret.s3ec2iam(action)._2.run
 }
