a914fe2f78454d66914db2c11f3df5fa9ae49f73
markhibberd <mark@hibberd.id.au>
2013-11-17 22:01:26 +1100

    More IAMRoles operations, starting to debug RoleLifecycleSpec.

--- src/main/scala/com/ambiata/saws/iam/IAMRoles.scala
+++ src/main/scala/com/ambiata/saws/iam/IAMRoles.scala
@@ -1,15 +1,22 @@
 package com.ambiata.saws
 package iam
 
-import com.amazonaws.services.ec2.AmazonEC2Client
-import com.amazonaws.services.ec2.model._
+import com.amazonaws.services.identitymanagement.model.{Role => AwsRole, _}
 import com.ambiata.saws.core._
 import com.ambiata.saws.iam._
 import scala.collection.JavaConverters._
 
-// FIX follow ensure/create pattern, add AwsLog.
 object IAMRoles {
+  // FIX follow ensure/create pattern, add AwsLog.
   def create(role: Role): IAMAction[Unit] =
-    AwsAction.attemptWithClient(client =>
-      IAM(client).createRole(role))
+    IAMAction(client => IAM(client).createRole(role))
+
+  def list: IAMAction[List[AwsRole]] =
+    IAMAction(_.listRoles.getRoles.asScala.toList)
+
+  def findByName(name: String): IAMAction[Option[AwsRole]] =
+    list.map(_.find(_.getRoleName == name))
+
+  def exists(name: String): IAMAction[Boolean] =
+    findByName(name).map(_.isDefined)
 }
