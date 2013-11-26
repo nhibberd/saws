1d647e3dafbb13d540e2120c617399842dad85a5
etorreborre <etorreborre@yahoo.com>
2013-11-26 13:11:01 +1100

    [\#59642062] added specialised versions of AwsActions

--- src/main/scala/com/ambiata/saws/core/AwsAction.scala
+++ src/main/scala/com/ambiata/saws/core/AwsAction.scala
@@ -99,63 +99,86 @@ case class AwsAction[A, +B](unsafeRun: A => (Vector[AwsLog], AwsAttempt[B])) {
 }
 
 object AwsAction {
-  def config[A]: AwsAction[A, A] =
-    AwsAction(a => (Vector(), AwsAttempt.ok(a)))
+  private def actions[A] = new AwsActions[A] {}
 
-  def ok[A, B](strict: B): AwsAction[A, B] =
-    value(strict)
-
-  def attempt[A, B](value: AwsAttempt[B]): AwsAction[A, B] =
-    AwsAction(_ => (Vector(), value))
-
-  def value[A, B](value: => B): AwsAction[A, B] =
-    AwsAction(_ => (Vector(), AwsAttempt.ok(value)))
-
-  def exception[A, B](t: Throwable): AwsAction[A, B] =
-    AwsAction(_ => (Vector(), AwsAttempt.exception(t)))
-
-  def fail[A, B](message: String): AwsAction[A, B] =
-    AwsAction(_ => (Vector(), AwsAttempt.fail(message)))
-
-  def error[A, B](message: String, t: Throwable): AwsAction[A, B] =
-    AwsAction(_ => (Vector(), AwsAttempt.error(message, t)))
-
-  def these[A, B](t: These[String, Throwable]): AwsAction[A, B] =
-    AwsAction(_ => (Vector(), AwsAttempt.these(t)))
-
-  def withClient[A, B](f: A => B): AwsAction[A, B] =
-    config[A].map(f)
-
-  def attemptWithClient[A, B](f: A => AwsAttempt[B]): AwsAction[A, B] =
-    config[A].attempt(f)
-
-  def log[A](message: AwsLog): AwsAction[A, Unit] =
-    AwsAction(_ => (Vector(message), AwsAttempt.ok(())))
-
-  def unlog[A](result: (Vector[AwsLog], AwsAttempt[A])): AwsAttempt[A] = result match {
-    case (log, attempt) => attempt
-  }
+  def config[A]: AwsAction[A, A]                                       = actions[A].config
+  def ok[A, B](strict: B): AwsAction[A, B]                             = actions[A].ok(strict)
+  def attempt[A, B](value: AwsAttempt[B]): AwsAction[A, B]             = actions[A].attempt(value)
+  def value[A, B](value: => B): AwsAction[A, B]                        = actions[A].value(value)
+  def exception[A, B](t: Throwable): AwsAction[A, B]                   = actions[A].exception(t)
+  def fail[A, B](message: String): AwsAction[A, B]                     = actions[A].fail(message)
+  def error[A, B](message: String, t: Throwable): AwsAction[A, B]      = actions[A].error(message, t)
+  def withClient[A, B](f: A => B): AwsAction[A, B]                     = actions[A].withClient(f)
+  def attemptWithClient[A, B](f: A => AwsAttempt[B]): AwsAction[A, B]  = actions[A].attemptWithClient(f)
+  def log[A](message: AwsLog): AwsAction[A, Unit]                      = actions[A].log(message)
+  def unlog[A](result: (Vector[AwsLog], AwsAttempt[A])): AwsAttempt[A] = actions[A].unlog(result)
 
   implicit def AwsActionMonad[A]: Monad[({ type l[a] = AwsAction[A, a] })#l] =
     new Monad[({ type L[a] = AwsAction[A, a] })#L] {
-      def point[B](v: => B) = AwsAction.value(v)
+      def point[B](v: => B) = AwsAction.ok(v)
       def bind[B, C](m: AwsAction[A, B])(f: B => AwsAction[A, C]) = m.flatMap(f)
     }
 }
 
-object S3Action {
+/**
+ * This trait is extended by S3, EC2, IAM to provide specialised versions of AwsActions
+ */
+trait AwsActions[A] {
+  def config: AwsAction[A, A] =
+    AwsAction(a => (Vector(), AwsAttempt.ok(a)))
+
+  def ok[B](strict: B): AwsAction[A, B] =
+    value(strict)
+
+  def attempt[B](value: AwsAttempt[B]): AwsAction[A, B] =
+    AwsAction(_ => (Vector(), value))
+
+  def value[B](value: => B): AwsAction[A, B] =
+    AwsAction(_ => (Vector(), AwsAttempt.ok(value)))
+
+  def exception[B](t: Throwable): AwsAction[A, B] =
+    AwsAction(_ => (Vector(), AwsAttempt.exception(t)))
+
+  def fail[B](message: String): AwsAction[A, B] =
+    AwsAction(_ => (Vector(), AwsAttempt.fail(message)))
+
+  def error[B](message: String, t: Throwable): AwsAction[A, B] =
+    AwsAction(_ => (Vector(), AwsAttempt.error(message, t)))
+
+  def these[B](t: These[String, Throwable]): AwsAction[A, B] =
+    AwsAction(_ => (Vector(), AwsAttempt.these(t)))
+
+  def withClient[B](f: A => B): AwsAction[A, B] =
+    config.map(f)
+
+  def attemptWithClient[B](f: A => AwsAttempt[B]): AwsAction[A, B] =
+    config.attempt(f)
+
+  def log(message: AwsLog): AwsAction[A, Unit] =
+    AwsAction(_ => (Vector(message), AwsAttempt.ok(())))
+
+  def unlog(result: (Vector[AwsLog], AwsAttempt[A])): AwsAttempt[A] = result match {
+    case (log, attempt) => attempt
+  }
+}
+
+object S3Action extends AwsActions[AmazonS3Client] {
   def apply[A](f: AmazonS3Client => A) =
     AwsAction.withClient(f)
 
-  implicit def S3ActionMonad: Monad[S3Action] = AwsAction.AwsActionMonad[AmazonS3Client]
+  def S3ActionMonad: Monad[S3Action] = AwsAction.AwsActionMonad[AmazonS3Client]
 }
 
-object EC2Action {
+object EC2Action extends AwsActions[AmazonEC2Client] {
   def apply[A](f: AmazonEC2Client => A) =
     AwsAction.withClient(f)
+
+  def EC2ActionMonad: Monad[EC2Action] = AwsAction.AwsActionMonad[AmazonEC2Client]
 }
 
-object IAMAction {
+object IAMAction extends AwsActions[AmazonIdentityManagementClient] {
   def apply[A](f: AmazonIdentityManagementClient => A) =
     AwsAction.withClient(f)
+
+  def IAMActionMonad: Monad[IAMAction] = AwsAction.AwsActionMonad[AmazonIdentityManagementClient]
 }
