76efb71bc77787935e4298a4e3727cc41a619648
markhibberd <mark@hibberd.id.au>
2013-11-26 12:51:20 +1100

    Add script as multipart encoding rather than raw mime message.

--- src/main/scala/com/ambiata/saws/ec2/EC2UserData.scala
+++ src/main/scala/com/ambiata/saws/ec2/EC2UserData.scala
@@ -5,11 +5,21 @@ import com.owtelse.codec.Base64
 
 object EC2UserData {
   def script(script: String) = {
-    val data = s"""MIME-Version: 1.0
-                  |Content-Type: text/x-shellscript
+    val data = s"""Content-Type: multipart/mixed; boundary="===============2633016704916973363=="
+                  |MIME-Version: 1.0
+                  |
+                  |--===============2633016704916973363==
+                  |Content-Type: text/x-shellscript; charset="us-ascii"
+                  |MIME-Version: 1.0
+                  |Content-Transfer-Encoding: 7bit
+                  |Content-Disposition: attachment; filename="bootstrap"
                   |
                   |#!/bin/sh
+                  |
                   |$script
+                  |
+                  |
+                  |--===============2633016704916973363==--
                   |""".stripMargin
     Base64.encode(data.getBytes("UTF-8"), "UTF-8")
   }
