77a58073b117c98d3fbae7b090044b3a1c1cc6c9
etorreborre <etorreborre@yahoo.com>
2014-02-27 15:50:18 +1100

    added a bunch of content types

--- src/main/scala/com/ambiata/saws/ses/ContentType.scala
+++ src/main/scala/com/ambiata/saws/ses/ContentType.scala
@@ -6,13 +6,23 @@ case class ContentType(value: String)
 object ContentType {
   object text {
     def plain = ContentType("text/plain")
-    def html = ContentType("text/html")
-    def csv = ContentType("text/csv")
-    def psv = ContentType("text/psv")
+    def html  = ContentType("text/html")
+    def csv   = ContentType("text/csv")
+    def psv   = ContentType("text/psv")
+  }
+  object image {
+    def jpeg = ContentType("image/jpeg")
+    def gif  = ContentType("image/gif")
+    def png  = ContentType("image/png")
+    def tiff = ContentType("image/tiff")
   }
   object application {
-    def excel = ContentType("application/vnd.ms-excel")
-    def json = ContentType("application/json")
-    def zip = ContentType("application/zip")
+    def excel      = ContentType("application/vnd.ms-excel")
+    def powerpoint = ContentType("application/vnd.ms-powerpoint")
+    def json       = ContentType("application/json")
+    def word       = ContentType("application/msword")
+    def zip        = ContentType("application/zip")
+    def gzip       = ContentType("application/x-gzip")
+    def pdf        = ContentType("application/pdf")
   }
 }
