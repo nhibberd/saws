64b62e70ab0a803fe8cb776b73613fb68159ff8c
markhibberd <mark@hibberd.id.au>
2014-04-14 13:53:41 +1000

    Add DistCopy functionality to saws.

---
+++ src/main/scala/com/ambiata/saws/emr/DistCopy.scala
@@ -0,0 +1,27 @@
+package com.ambiata.saws
+package emr
+
+import com.amazonaws.services.elasticmapreduce.model.ActionOnFailure
+import com.ambiata.saws.tooling._
+import scalaz._, Scalaz._
+
+object DistCopy {
+  def run(clusterId: String, args: List[String]) = for {
+    _       <- EMR.describeCluster(clusterId)
+    stepIds <- EMR.addSteps(clusterId, List(step(args)))
+    _       <- Wait.waitFor(
+                 EMR.describeStep(clusterId, stepIds.head).map(s => List("COMPLETED", "CANCELLED", "FAILED", "INTERRUPTED").exists(s.getState === _)),
+                    s"Running S3DistCp as step '${stepIds.head}'".some,
+                    15000)
+    status  <- EMR.describeStep(clusterId, stepIds.head)
+  } yield status
+
+  def step(args: List[String]): Step = Step(
+    "weiv-distcp",
+    ActionOnFailure.CONTINUE,
+    "s3://elasticmapreduce/libs/s3distcp/role/s3distcp.jar",
+    "",
+    args,
+    Map.empty
+  )
+}
