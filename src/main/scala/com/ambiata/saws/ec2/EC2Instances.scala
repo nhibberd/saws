48bd37a2490b9e0b49079d6140f116346b3a51fe
markhibberd <mark@hibberd.id.au>
2014-06-16 16:46:26 +1000

    Support spot prices.

--- src/main/scala/com/ambiata/saws/ec2/EC2Instances.scala
+++ src/main/scala/com/ambiata/saws/ec2/EC2Instances.scala
@@ -13,19 +13,22 @@ import scalaz._, Scalaz._
 import scala.concurrent._
 
 object EC2Instances {
-  def run(env: String, image: EC2Image, count: Int, keypair: Option[String]): EC2Action[Reservation] = for {
+  def run(env: String, image: EC2Image, count: Int, keypair: Option[String], bid: Option[String]): EC2Action[List[String]] = for {
     subnet      <- image.vpc.traverse(EC2Subnets.findByVpc).map(_.flatten)
     group       <- EC2SecurityGroups.findByNameOrFail(image.group, image.vpc)
-    reservation <- start(image, group, subnet, count, keypair)
-    _           <- EC2Tags.tags(reservation.getInstances.asScala.toList.map(_.getInstanceId), image.tags ++ List(
+    ids         <- bid match {
+      case None => start(image, group, subnet, count, keypair)
+      case Some(b) => spot(image, group, subnet, count, keypair, b)
+    }
+    _           <- EC2Tags.tags(ids, image.tags ++ List(
                      "Name" -> s"${env}.${image.flavour}.${EC2Tags.stamp}"
                    ))
-    _           <- EC2Status.waitForReady(reservation.getInstances.asScala.toList.map(_.getInstanceId))
-    _           <- associate(image, reservation)
+    _           <- EC2Status.waitForReady(ids)
+    _           <- associate(image, ids)
     _           <- AwsLog.StartInstance(image.flavour).log
-  } yield reservation
+  } yield ids
 
-  def start(image: EC2Image, group: AwsSecurityGroup, subnet: Option[Subnet], count: Int, keypair: Option[String]): EC2Action[Reservation] =
+  def start(image: EC2Image, group: AwsSecurityGroup, subnet: Option[Subnet], count: Int, keypair: Option[String]): EC2Action[List[String]] =
     EC2Action(client =>
       client.runInstances({
         val r = new RunInstancesRequest(image.ami, count, count)
@@ -41,7 +44,29 @@ object EC2Instances {
         image.configure.foreach(script => r.setUserData(EC2UserData.script(script)))
 
         r
-      }).getReservation)
+      }).getReservation.getInstances.asScala.toList.map(_.getInstanceId))
+
+  def spot(image: EC2Image, group: AwsSecurityGroup, subnet: Option[Subnet], count: Int, keypair: Option[String], bid: String): EC2Action[List[String]] =
+    EC2Action(client =>
+      client.requestSpotInstances(
+        new RequestSpotInstancesRequest(bid)
+          .withInstanceCount(count)
+          .withLaunchSpecification({
+            val spec = new LaunchSpecification()
+              .withImageId(image.ami)
+              .withInstanceType(image.size.size)
+              .withSecurityGroups(List(group.getGroupId).asJava)
+              .withBlockDeviceMappings(image.size.devices.mapping.map({
+                case (dev, virt) => new BlockDeviceMapping().withDeviceName(dev).withVirtualName(virt)
+              }).asJava)
+            keypair.foreach(spec.setKeyName)
+            image.profile.foreach(p => spec.setIamInstanceProfile(new IamInstanceProfileSpecification().withName(p.name)))
+            subnet.foreach(s => spec.setSubnetId(s.getSubnetId))
+            image.configure.foreach(script => spec.setUserData(EC2UserData.script(script)))
+            spec
+          })
+      ).getSpotInstanceRequests.asScala.toList.map(_.getInstanceId))
+
 
   def list: EC2Action[List[Instance]] =
     EC2Action(client =>
@@ -68,12 +93,12 @@ object EC2Instances {
       case Some(i) => i.pure[EC2Action]
     })
 
-  def associate(image: EC2Image, reservation: Reservation): EC2Action[Unit] =
-    (image.elasticIp, reservation.getInstances.asScala.toList) match {
+  def associate(image: EC2Image, ids: List[String]): EC2Action[Unit] =
+    (image.elasticIp, ids) match {
       case (None, _) =>
         ().pure[EC2Action]
       case (Some(ip), List(instance)) if image.cardinality == SingletonEC2Image =>
-        EC2IPs.associate(ip, instance.getInstanceId).as(())
+        EC2IPs.associate(ip, instance).as(())
       case (Some(_),  _) =>
         EC2Action.fail[Unit]("Something went wrong or is misconfigured, can't associate IP with multiple machines.")
     }
