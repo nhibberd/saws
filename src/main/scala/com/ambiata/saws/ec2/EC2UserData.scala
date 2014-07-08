5567c37df47e19f2c690ac8f6a8781ec827be8aa
Ben Lever <ben.lever@nicta.com.au>
2014-07-08 17:56:21 +1000

    Handle cloud-config run cmds with args.

--- src/main/scala/com/ambiata/saws/ec2/EC2UserData.scala
+++ src/main/scala/com/ambiata/saws/ec2/EC2UserData.scala
@@ -5,9 +5,10 @@ import com.owtelse.codec.Base64
 
 object EC2UserData {
   def script(script: String) = {
+    val cmd = script.split(' ').toList.map(t => "\"" + t + "\"")
     val data = s"""#cloud-config
                   |runcmd:
-                  | - [ "$script" ]""".stripMargin
+                  | - [ ${cmd.mkString(", ")} ]""".stripMargin
     Base64.encode(data.getBytes("UTF-8"), "UTF-8")
   }
 }
