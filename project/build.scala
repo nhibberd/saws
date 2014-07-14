f09e75f9c41aabde43a9a73ab930f08325029ea2
Eric Torreborre <etorreborre@yahoo.com>
2014-07-14 17:55:28 +1000

    split the project into several modules. fixes #19

--- project/build.scala
+++ project/build.scala
@@ -8,18 +8,66 @@ object build extends Build {
   lazy val saws = Project(
     id = "saws",
     base = file("."),
-    settings = Defaults.defaultSettings ++
-               projectSettings          ++
-               compilationSettings      ++
-               testingSettings          ++
-               packageSettings
-    )
+    settings = standardSettings ++ promulgate.library("com.ambiata.saws", "ambiata-oss")
+    ).dependsOn(core, ec2, s3, emr, ses, testing)
+
+  lazy val standardSettings = Defaults.defaultSettings ++
+                   projectSettings          ++
+                   compilationSettings      ++
+                   testingSettings          ++
+                   Seq(resolvers ++= depend.resolvers)
 
   lazy val projectSettings: Seq[Settings] = Seq(
-    name := "saws",
-    version in ThisBuild := "1.2.1",
-    organization := "com.ambiata",
-    scalaVersion := "2.10.3")
+      name := "saws"
+    , version in ThisBuild := "1.2.1"
+    , organization := "com.ambiata"
+    , scalaVersion := "2.10.4"
+  ) ++ Seq(prompt)
+
+  lazy val core = Project(
+    id = "core"
+  , base = file("saws-core")
+  , settings = standardSettings ++ lib("core") ++ Seq[Settings](
+      name := "saws-core"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.aws ++ depend.scalazStream ++ depend.mundane ++ depend.mundaneTesting ++ depend.testing)
+  )
+
+  lazy val iam = Project(
+    id = "iam"
+  , base = file("saws-iam")
+  , settings = standardSettings ++ lib("iam") ++ Seq[Settings](name := "saws-iam")
+  ).dependsOn(core)
+
+  lazy val ec2 = Project(
+    id = "ec2"
+  , base = file("saws-ec2")
+  , settings = standardSettings ++ lib("ec2") ++ Seq[Settings](name := "saws-ec2")
+  ).dependsOn(core, iam)
+
+  lazy val s3 = Project(
+    id = "s3"
+  , base = file("saws-s3")
+  , settings = standardSettings ++ lib("s3") ++ Seq[Settings](name := "saws-s3") 
+  ).dependsOn(core)
+
+  lazy val emr = Project(
+    id = "emr"
+  , base = file("saws-emr")
+  , settings = standardSettings ++ lib("emr") ++ Seq[Settings](name := "saws-emr")
+  ).dependsOn(core)
+
+  lazy val ses = Project(
+    id = "ses"
+  , base = file("saws-ses")
+  , settings = standardSettings ++ lib("ses") ++ Seq[Settings](name := "saws-ses")
+  ).dependsOn(core)
+
+  lazy val testing = Project(
+    id = "testing"
+  , base = file("saws-testing")
+  , settings = standardSettings ++ lib("testing") ++ Seq[Settings](name := "saws-testing"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.specs2 ++ depend.ssh)
+  ).dependsOn(iam, emr, ec2, ses, s3)
 
   lazy val compilationSettings: Seq[Settings] = Seq(
     javacOptions ++= Seq("-Xmx3G", "-Xms512m", "-Xss4m"),
@@ -37,4 +85,13 @@ object build extends Build {
     cancelable := true,
     javaOptions += "-Xmx3G"
   )
+
+  def lib(name: String) =
+    promulgate.library(s"com.ambiata.saws.$name", "ambiata-oss")
+
+  lazy val prompt = shellPrompt in ThisBuild := { state =>
+    val name = Project.extract(state).currentRef.project
+    (if (name == "saws") "" else name) + "> "
+  }
+
 }
---
+++ saws-testing/src/main/scala/com/ambiata/saws/iam/RoleLifecycleSpec.scala
@@ -0,0 +1,64 @@
+package com.ambiata
+package saws
+package iam
+
+import scalaz._, Scalaz._
+import org.specs2.specification._
+import org.specs2.matcher._
+import mundane.control.Result
+import mundane.testing.ResultMatcher._
+import testing._
+
+
+class RoleLifecycleSpec extends IntegrationSpec with BeforeAfterExample with ThrownExpectations { def is = sequential ^ s2"""
+
+  Role Lifecycle
+  ==============
+  - can create an IAM role that doesn't already exist $e1
+  - can create an IAM role that already exists $e2
+  - can not delete an IAM role that doesn't already exist $e3
+  - can delete an IAM role that already exists $e4
+
+"""
+
+  val role = Role("role-lifecycle-spec", Nil)
+  lazy val iam = IAM()
+
+  def e1 = {
+    val steps =
+      iam.createRole(role) >>
+      iam.roleExists(role.name)
+    steps must beOk
+    steps.toEither must beRight(true)
+  }
+
+  def e2 = {
+    val steps =
+      iam.createRole(role).replicateM(2) >>
+      iam.roleExists(role.name)
+    steps must beOk
+    steps.toEither must beRight(true)
+  }
+
+  def e3 = {
+    iam.deleteRole(role.name) must beOk.not
+    iam.roleExists(role.name).toEither must beRight(false).eventually(retries = 8, sleep = 5.seconds)
+  }.pendingUntilFixed("This breaks with timing issues, needs further investigation")
+
+  def e4 = {
+    val steps =
+      (iam.createRole(role) >>
+      iam.deleteRole(role.name) >>
+      iam.roleExists(role.name)).map(println)
+    steps must beOk
+    steps.toEither must beRight(false)
+  }.pendingUntilFixed("This breaks with timing issues, needs further investigation")
+
+  def before {
+    iam.deleteRole(role.name)
+  }
+
+  def after {
+    iam.deleteRole(role.name)
+  }
+}
