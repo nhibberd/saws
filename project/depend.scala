b0c998d5526662e81394ba8c9d456cfdb29a8d2f
Huw Campbell <huw.campbell@ambiata.com>
2014-10-27 13:45:35 +1100

    Bump mundane

--- project/depend.scala
+++ project/depend.scala
@@ -7,7 +7,7 @@ object depend {
 
   val scalazStream = Seq("org.scalaz.stream" %% "scalaz-stream" % "0.5a")
 
-  val mundaneVersion = "1.2.1-20141016221505-82db433"
+  val mundaneVersion = "1.2.1-20141024050817-e2b01b2"
 
   val mundane = Seq(  "com.ambiata" %% "mundane-io"
                     , "com.ambiata" %% "mundane-control").map(_ % mundaneVersion)
