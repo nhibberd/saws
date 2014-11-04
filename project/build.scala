61552b794067af1fb11568a4ca88b2156b8e3d95
Nick Hibberd <nhibberd@gmail.com>
2014-11-04 15:25:29 +1100

    Adding shim project

--- project/build.scala
+++ project/build.scala
@@ -12,13 +12,14 @@ object build extends Build {
     id = "saws",
     base = file("."),
     settings = standardSettings ++ promulgate.library("com.ambiata.saws", "ambiata-oss"),
-    aggregate = Seq(core, ec2, s3, iam, emr, ses, testing)
-    ).dependsOn(core, ec2, s3, iam, emr, ses)
+    aggregate = Seq(core, ec2, s3, iam, emr, ses, testing, shim)
+    ).dependsOn(core, ec2, s3, iam, emr, ses, shim)
 
   lazy val standardSettings = Defaults.coreDefaultSettings ++
                    projectSettings          ++
                    compilationSettings      ++
                    testingSettings          ++
+                   proguard                 ++
                    Seq(resolvers ++= depend.resolvers)
 
   lazy val projectSettings: Seq[Settings] = Seq(
@@ -29,13 +30,44 @@ object build extends Build {
     , crossScalaVersions := Seq(scalaVersion.value)
   ) ++ Seq(prompt)
 
+  /*
+   Hadoop has an imcompatible dependency on `aws-java-sdk` with the version in use here.
+   Therefore we are using Proguard to map the package names in `aws-java-sdk` in use
+   here to avoid runtime errros.
+   ProguardPre will create a mapping file of the `aws-java-sdk` and `Mappings.mapping`
+   will then filter and rename the mappings file before the final Proguard task runs the
+   mappings over the library.
+   */
+  val ProguardPre = config("proguard-pre")
+
+  def dependenciesPre: Seq[Setting[_]] = Seq(
+    ivyConfigurations += ProguardPre,
+    libraryDependencies <+= (ProguardKeys.proguardVersion in ProguardPre) { version =>
+      "net.sf.proguard" % "proguard-base" % version % ProguardPre.name
+    }
+  )
+
+  val proguard =
+    proguardSettings         ++
+    inConfig(ProguardPre)(ProguardSettings.default ++ dependenciesPre ++ Seq(managedClasspath <<= (managedClasspath, managedClasspath in Compile).map({ case (y, x) => y ++ x })) )       ++
+    dependenciesPre          ++
+    Seq[Settings](
+        ProguardKeys.options in ProguardPre <<= (update, packageBin in Compile).map({ case (u, b) => Mappings.premapping(u, b) })
+      , ProguardKeys.options in Proguard <<= (ProguardKeys.proguard in ProguardPre, name, version, update, packageBin in Compile).map({
+          case(_, n, v, u, b) => Mappings.mapping(n, v, u, b)
+        })
+      , javaOptions in (Proguard, ProguardKeys.proguard) := Seq("-Xmx2G")
+    )                        ++
+    addArtifact(name.apply(n => Artifact(s"$n-shade", "shade", "jar")), (ProguardKeys.proguard in Proguard, packageBin in Compile, name, version).map({ case (_, s, n, v) =>
+      s.getParentFile / "proguard" / s"$n-proguard-$v.jar"}))
+
   lazy val core = Project(
     id = "core"
   , base = file("saws-core")
   , settings = standardSettings ++ lib("core") ++ Seq[Settings](
       name := "saws-core"
     ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.aws ++ depend.scalazStream ++ depend.mundane ++ depend.testing)
-  )
+  ).dependsOn(shim)
 
   lazy val iam = Project(
     id = "iam"
@@ -49,39 +81,10 @@ object build extends Build {
   , settings = standardSettings ++ lib("ec2") ++ Seq[Settings](name := "saws-ec2")
   ).dependsOn(core, iam)
 
-  val ProguardPre = config("proguard-pre")
-
-  def dependenciesPre: Seq[Setting[_]] = Seq(
-    ivyConfigurations += ProguardPre,
-    libraryDependencies <+= (ProguardKeys.proguardVersion in ProguardPre) { version =>
-      "net.sf.proguard" % "proguard-base" % version % ProguardPre.name
-    }
-  )
-
-  /*
-   Hadoop has an imcompatible dependency on `aws-java-sdk` with the version in use here.
-   Therefore we are using Proguard to map the package names in `aws-java-sdk` in use
-   here to avoid runtime errros.
-   ProguardPre will create a mapping file of the `aws-java-sdk` and `Mappings.mapping`
-   will then filter and rename the mappings file before the final Proguard task runs the
-   mappings over the library.
-   */
   lazy val s3 = Project(
     id = "s3"
   , base = file("saws-s3")
-  , settings = standardSettings ++ proguardSettings ++ lib("s3")
-      ++ inConfig(ProguardPre)(ProguardSettings.default ++ dependenciesPre ++ Seq(managedClasspath <<= (managedClasspath, managedClasspath in Compile).map({ case (y, x) => y ++ x })) )
-      ++ dependenciesPre
-      ++ Seq[Settings](
-          name := "saws-s3"
-        , ProguardKeys.options in ProguardPre <<= (update, packageBin in Compile).map({ case (u, b) => Mappings.premapping(u, b) })
-        , ProguardKeys.options in Proguard <<= (ProguardKeys.proguard in ProguardPre, name, version, update, packageBin in Compile).map({
-            case(_, n, v, u, b) => Mappings.mapping(n, v, u, b)
-          })
-      , javaOptions in (Proguard, ProguardKeys.proguard) := Seq("-Xmx2G")
-      )
-    ++ addArtifact(name.apply(n => Artifact(s"$n-shade", "shade", "jar")), (ProguardKeys.proguard in Proguard, packageBin in Compile, name, version).map({ case (_, s, n, v) =>
-      s.getParentFile / "proguard" / s"$n-proguard-$v.jar"}))
+  , settings = standardSettings ++ lib("s3") ++ Seq[Settings](name := "saws-s3")
   ).dependsOn(core)
 
   lazy val emr = Project(
@@ -102,6 +105,23 @@ object build extends Build {
   , settings = standardSettings ++ lib("testing") ++ Seq[Settings](name := "saws-testing"
     ) ++ Seq[Settings](libraryDependencies ++= depend.specs2 ++ depend.ssh ++ depend.mundane ++ depend.mundaneTesting)
   ).dependsOn(iam, emr, ec2, ses, s3)
+
+  lazy val shim = Project(
+    id = "shim"
+  , base = file("saws-shim")
+  , settings = projectSettings ++ compilationSettings ++ Seq(resolvers ++= depend.resolvers) ++ lib("shim") ++ proguardSettings ++
+      Seq[Settings](libraryDependencies ++= depend.aws) ++
+      inConfig(ProguardPre)(ProguardSettings.default ++ dependenciesPre ++ Seq(managedClasspath <<= (managedClasspath, managedClasspath in Compile).map({ case (y, x) => y ++ x}))) ++
+      dependenciesPre ++q
+      Seq[Settings](name := "saws-shim"
+        , ProguardKeys.options in ProguardPre <<= (update, packageBin in Compile).map({ case (u, _) => Mappings.preshim(u) })
+        , ProguardKeys.options in Proguard <<= (ProguardKeys.proguard in ProguardPre, name, version, update, packageBin in Compile).map({
+            case(_, n, v, u, b) => Mappings.shim(n, v, u, b)
+          })
+      , javaOptions in (Proguard, ProguardKeys.proguard) := Seq("-Xmx2G")
+      ) ++
+      addArtifact(name.apply(n => Artifact(s"$n-shade", "shade", "jar")), (ProguardKeys.proguard in Proguard, packageBin in Compile, name, version).map({ case (_, s, n, v) => s.getParentFile / "proguard" / s"$n-proguard-$v.jar"}))
+  )
 
   lazy val compilationSettings: Seq[Settings] = Seq(
     javacOptions ++= Seq("-Xmx3G", "-Xms512m", "-Xss4m"),
--- project/mappings.scala
+++ project/mappings.scala
@@ -21,8 +21,8 @@ object Mappings {
     injar(file) ++
     keepAmbiata ++
     Seq("-applymapping aws.map") ++
-    setup(file, update) ++
     Seq(s"-outjars ${name}-proguard-${version}.jar") ++
+    setup(file, update) ++
     dont
   }
 
@@ -42,5 +42,33 @@ object Mappings {
 
   val dont =
     Seq("-dontoptimize", "-dontshrink", "-dontpreverify")
+
+  def preshim(update: UpdateReport) = {
+    Seq(update.select(module = moduleFilter(organization = "com.amazonaws")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq("-ignorewarnings"
+      , "-printmapping mappings.map"
+      , "-keep class com.amazonaws.** { *; }"
+      , "-keepclassmembers class com.amazonaws.** { *; }"
+      , "-libraryjars <java.home>/lib/rt.jar") ++
+    Seq(update.select(module = moduleFilter(organization = "com.amazonaws")).map(f => s"-libraryjars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq("-outjars empty.jar") ++
+    dont
+  }
+
+  def shim(name: String, version: String, update: UpdateReport, file: File) = {
+    val r = IO.readLines(file.getParentFile / "proguard" / "mappings.map")
+    val s = r.filter(!_.startsWith(" "))
+    val t = s.map(_.replace("-> com.amazonaws", "-> com.ambiata.com.amazonaws"))
+    IO.writeLines(file.getParentFile / "proguard" / "aws.map", t)
+    Seq(update.select(module = moduleFilter(organization = "com.amazonaws")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq("-ignorewarnings"
+      , "-keep class com.amazonaws.** { *; }"
+      , "-keepclassmembers class com.amazonaws.** { *; }"
+      , "-applymapping aws.map"
+      , "-libraryjars <java.home>/lib/rt.jar") ++
+    Seq(update.select(module = moduleFilter(organization = "com.amazonaws")).map(f => s"-libraryjars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq(s"-outjars ${name}-proguard-${version}.jar") ++
+    dont
+  }
 
 }
