89aeaf489dcfbe7e127ac48af31e657b89282842
mth <mark@hibberd.id.au>
2015-10-05 21:04:09 +1100

    Remove ambiata specific account alias.

--- saws-iam/src/main/scala/com/ambiata/saws/iam/IAMAlias.scala
+++ saws-iam/src/main/scala/com/ambiata/saws/iam/IAMAlias.scala
@@ -10,10 +10,4 @@ import scala.collection.JavaConverters._
 object IAMAlias {
   def list: IAMAction[List[String]] = IAMAction(client =>
     client.listAccountAliases.getAccountAliases.asScala.toList)
-
-  def isAmbiata: IAMAction[Boolean] =
-    list.map(_.contains("ambiata"))
-
-  def isAmbiataProd: IAMAction[Boolean] =
-    list.map(_.contains("ambiata-prod"))
 }
